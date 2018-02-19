4fe07f83fcdc8838b771dbf812c63f02f6bae14b
Bala Krishna Ragala <baluragala@gmail.com>
2018-02-19 21:15:30 +0530

    forms done

--- e-store/src/app/products/add-product-reactive/add-product-reactive.component.ts
+++ e-store/src/app/products/add-product-reactive/add-product-reactive.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from "@angular/forms";
 
 @Component({
   selector: "app-add-product-reactive",
@@ -8,21 +13,37 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
 })
 export class AddProductReactiveComponent implements OnInit {
   productForm: FormGroup;
-  constructor() {}
+  //private formBuilder;
+  constructor(private formBuilder: FormBuilder) {
+    //this.formBuilder = formBuilder;
+  }
 
   ngOnInit() {
-    this.productForm = new FormGroup({
-      title: new FormControl("pixel", [
-        Validators.required,
-        Validators.minLength(5),
-        Validators.maxLength(10)
-      ]),
-      price: new FormControl(),
-      stock: new FormControl()
+    // this.productForm = new FormGroup({
+    //   title: new FormControl("pixel", [
+    //     Validators.required,
+    //     Validators.minLength(5),
+    //     Validators.maxLength(10)
+    //   ]),
+    //   price: new FormControl(),
+    //   stock: new FormControl()
+    // });
+    this.productForm = this.formBuilder.group({
+      title: [
+        "pixel",
+        [Validators.required, Validators.minLength(5), Validators.maxLength(10)]
+      ],
+      price: "",
+      stock: ""
     });
   }
 
   onSubmit() {
     console.log(this.productForm.value);
+  }
+
+  load() {
+    //this.productForm.setValue({ title: "iphone", price: 600, stock: 500 });
+    this.productForm.patchValue({ tite: "iphone", price: 600, stock: 500 });
   }
 }
